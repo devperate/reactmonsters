144e6dc3fc770a01d61d1e12907cfbed37b3dfca
Gerardo Hernandez Rodriguez <gerardo.hrodriguez@DTVC.VCL.ETECSA.CU>
2020-07-18 17:07:06 -0500

    Updates

---
+++ precache-manifest.7900a1f3bcb1add9ddda8a0866006a3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e82b5a6fe5b7019d69ce01833c290960",
+    "url": "/reactmonsters/index.html"
+  },
+  {
+    "revision": "8e1c869e86fea8e7b770",
+    "url": "/reactmonsters/static/css/main.5e3aac01.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/reactmonsters/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactmonsters/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e1c869e86fea8e7b770",
+    "url": "/reactmonsters/static/js/main.42886863.chunk.js"
+  },
+  {
+    "revision": "b601615b31874966c64f",
+    "url": "/reactmonsters/static/js/runtime-main.9a4fa32b.js"
+  }
+]);
