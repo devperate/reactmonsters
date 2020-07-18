5107527bff8a00c86f906684fe2e0fea01746df3
Gerardo Hernandez Rodriguez <gerardo.hrodriguez@DTVC.VCL.ETECSA.CU>
2020-07-18 17:18:33 -0500

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+//import logo from './logo.svg';
+import {CardList} from './component/card-list/card-list.comp';
 import './App.css';
+import {SearchBox} from './component/search-box/search-box.comp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {  
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: [],
+      searchText : ''
+    };
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/users.json')
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}));
+  }
+
+  searchHandler = (e) => {
+    this.setState({ searchText: e.target.value})
+  }
+
+  render(){
+    const {monsters, searchText} = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchText.toLowerCase()));
+    
+    return (
+      <div className="App">
+        <h1>monsters mf</h1>
+        <SearchBox placeholder='search something mf...' changeHandler={this.searchHandler} />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }  
 }
 
 export default App;
